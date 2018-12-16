23c2d7de06d56fb0d1dd4e1135115c0addc80acc
Fabian Tamp <fabian.tamp@gmail.com>
2018-12-16 15:33:49 +0100

    Fix longstanding test failures :-/

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,13 @@
+import json
+
 from api import entry
 
 
 def test_api():
-    obj = entry.haiku_handler(None, None)
+    obj = entry.haiku_handler({}, None)
     # Should be 3 lines of text
-    assert obj['emoji'].count('\n') == 2
-    assert obj['descriptions'].count('\n') == 2
+    body = json.loads(obj['body'])
+    assert body['emoji'].count('\n') == 2
+    assert body['descriptions'].count('\n') == 2
+    assert obj['statusCode'] == 200
+    assert 'Access-Control-Allow-Origin' in obj['headers']
